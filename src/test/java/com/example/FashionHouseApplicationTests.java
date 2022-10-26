2e23ec8e49284838c5d3f5ad34c26dd9ba005760
Upasana Upasana <upasana.upasana@publicissapient.com>
2022-10-26 17:22:06 +0530

    first commit

---
+++ src/test/java/com/example/FashionHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
